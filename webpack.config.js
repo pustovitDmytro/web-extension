0a66e64ccde8a9391a2781018c29e6955df8850d
dmytroPustovit <pustic@gmail.com>
2017-07-29 22:50:26 +0300

    webpack is workisng

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,52 @@
-const path = require("path");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const styles = require('./webpack/styles');
+const uglifyJS = require('./webpack/uglify');
+const images = require('./webpack/images');
 
-module.exports = {
-    entry: {
-        background: "./src/background/index.js",
-        popup: "./src/popup/index.js",
-        content: "./src/content/index.js"
+const PATHS = {
+    source: path.join(__dirname, 'src'),
+    build: path.join(__dirname, 'build')
+};
+
+const common = merge([
+    {
+        entry: {
+            background: "./src/background/index.js",
+            popup: "./src/popup/index.js",
+            content: "./src/content/index.js"
+        },
+        output: {
+            path: path.resolve(__dirname, "build"),
+            filename: "[name].js"
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: PATHS.build + '/popup/index.html',
+                template: PATHS.source + '/popup/index.html',
+                chunks: ["popup"]
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'common'
+            })
+        ]
     },
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "[name]/index.js"
+    styles(),
+    images()
+]);
+
+module.exports = function(env) {
+    if (env === 'chrome'){
+        return merge([
+            common,
+            uglifyJS()
+        ]);
+    }
+    if (env === 'firefox'){
+        return merge([
+            common
+        ])
     }
 };
---
+++ webpack/images.js
@@ -0,0 +1,18 @@
+/**
+ * Created by pusti on 29.07.2017.
+ */
+module.exports = function() {
+    return {
+        module: {
+            rules: [
+                {
+                    test: /\.(jpg|png|svg)$/,
+                    loader: 'file-loader',
+                    options: {
+                        name: 'images/[name].[ext]'
+                    },
+                },
+            ],
+        },
+    };
+};
