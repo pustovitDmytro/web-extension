a22f08e900dad19c7409e93353622beff134bba9
dmytroPustovit <pustic@gmail.com>
2017-07-29 21:18:27 +0300

    webpack base

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require("path");
 
 module.exports = {
     entry: {
-        background_scripts: "./background_scripts/background.js",
-        popup: "./popup/left-pad.js"
+        background: "./src/background/index.js",
+        popup: "./src/popup/index.js"
     },
     output: {
-        path: path.resolve(__dirname, "addon"),
+        path: path.resolve(__dirname, "build"),
         filename: "[name]/index.js"
     }
 };
